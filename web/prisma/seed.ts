2f6256067a6fff2e5496b1433dfdb3cec1a91d66
ANLU corporations <41836322+anlucorporations@users.noreply.github.com>
2026-08-05 21:00:31 -0400

    fix: Asignar wallet 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 al SuperUsuario anlu

--- web/prisma/seed.ts
+++ web/prisma/seed.ts
@@ -17,7 +17,7 @@ async function main() {
     nombre: "anlu",
     cedula: "V-12533620",
     correo: "anlucorporations@gmail.com",
-    walletAddress: "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720", // Anvil Account #9 (anlu)
+    walletAddress: "0xf39Fd6e51aad88F6F4ceaB8827279cffFb92266", // SuperUsuario anlu
     cargo: Cargo.PRESIDENTE,
     telefono: "+584120358824",
     direccion: "Sede Principal Cooperativa Los Cappones",
